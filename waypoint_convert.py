421221a9144cb25b18472c32e450be6b025e020c
af0ns0b <165789550+af0ns0b@users.noreply.github.com>
2024-06-21 10:06:16 +0100

    Update waypoint_convert.py

--- waypoint_convert.py
+++ waypoint_convert.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import rospy
 import math
 
@@ -16,57 +18,63 @@ def rightCB(r):
   right_x,right_y = projection(r.longitude, r.latitude)
  
 def pointCB(p):
-  goal = Point()
-  goal.x,goal.y = projection(p.longitude, p.latitude)
-  goal.z = 0
-  waypoint_pub.publish(goal)
+  way = Point()
+  way.x,way.y = projection(p.longitude, p.latitude)
+  way.z = 0
+  waypoint_pub.publish(way)
 
-# Init ROS node
-rospy.init_node('waypoint_convert')
+def main():
+  # Init ROS node
+  rospy.init_node('waypoint_convert')
 
-# Publishers
-waypoint_pub = rospy.Publisher('waypoint/goal', Point, queue_size = 10)
-pose_pub = rospy.Publisher('robot_pose', Pose2D, queue_size = 10)
-robot_gps_pub = rospy.Publisher('robot_gps_pose', NavSatFix, queue_size = 10)
-robot_heading_pub = rospy.Publisher('robot_heading', Float32, queue_size = 10)
+  # Publishers
+  waypoint_pub = rospy.Publisher('waypoint/goal', Point, queue_size = 10)
+  pose_pub = rospy.Publisher('robot_pose', Pose2D, queue_size = 10)
+  robot_gps_pub = rospy.Publisher('robot_gps_pose', NavSatFix, queue_size = 10)
+  robot_heading_pub = rospy.Publisher('robot_heading', Float32, queue_size = 10)
 
-# Subscribers
-leftGOT_sub = rospy.Subscriber('waypoint/left_sensor', NavSatFix, leftCB)
-rightGOT_sub = rospy.Subscriber('waypoint/right_sensor', NavSatFix, rightCB)
-point_sub = rospy.Subscriber('waypoint', NavSatFix, pointCB)
+  # Subscribers
+  leftGOT_sub = rospy.Subscriber('waypoint/left_sensor', NavSatFix, leftCB)
+  rightGOT_sub = rospy.Subscriber('waypoint/right_sensor', NavSatFix, rightCB)
+  point_sub = rospy.Subscriber('waypoint', NavSatFix, pointCB)
 
-rate = rospy.Rate(1000)
+  rate = rospy.Rate(1000)
 
-# Variables
-global left_x,left_y,right_x,right_y
-left_x = 0.0
-left_y = 0.0
-right_x = 0.0
-right_y = 0.0
+  # Variables
+  global left_x,left_y,right_x,right_y
+  left_x = 0.0
+  left_y = 0.0
+  right_x = 0.0
+  right_y = 0.0
 
-projection = Proj(proj="utm", zone="34", ellps='WGS84')
+  projection = Proj(proj="utm", zone="29", ellps='WGS84')
 
-while not rospy.is_shutdown():
-  # Publish robot position in projection coordinate
-  pose = Pose2D()
-  gpspose = NavSatFix()
-  pose.x = (left_x + right_x) / 2.0
-  pose.y = (left_y + right_y) / 2.0
-  pose.theta = math.atan2( (right_y-left_y), (right_x-left_x) ) + math.pi / 2.0
-  if pose.theta > math.pi:
-    pose.theta = pose.theta - 2.0 * math.pi
-  pose_pub.publish(pose)
+  while not rospy.is_shutdown():
+   # Publish robot position in projection coordinate
+    pose = Pose2D()
+    gpspose = NavSatFix()
+    pose.x = (left_x + right_x) / 2.0
+    pose.y = (left_y + right_y) / 2.0
+    pose.theta = math.atan2( (right_y-left_y), (right_x-left_x) ) + math.pi / 2.0
+    if pose.theta > math.pi:
+     pose.theta = pose.theta - 2.0 * math.pi
+    pose_pub.publish(pose)
   
-  # Publish robot position in GPS
-  gpspose.longitude,gpspose.latitude = projection(pose.x,pose.y,inverse=True)
-  robot_gps_pub.publish(gpspose)
+    # Publish robot position in GPS
+    gpspose.longitude,gpspose.latitude = projection(pose.x,pose.y,inverse=True)
+    robot_gps_pub.publish(gpspose)
   
-  # Publish robot heading, 0-359 degrees, North: 0, East: 90
-  heading_rad =-math.atan2( (right_y-left_y), (right_x-left_x) )
-  if heading_rad < 0:
-    heading_rad = heading_rad + 2.0 * math.pi
-  heading = math.fabs(heading_rad) / math.pi * 180.0
-  robot_heading_pub.publish(heading)
+    # Publish robot heading, 0-359 degrees, North: 0, East: 90
+    heading_rad =-math.atan2( (right_y-left_y), (right_x-left_x) )
+    if heading_rad < 0:
+      heading_rad = heading_rad + 2.0 * math.pi
+    heading = math.fabs(heading_rad) / math.pi * 180.0
+    robot_heading_pub.publish(heading)
   
-  rate.sleep()
+    rate.sleep()
   
+if __name__ == '__main__':
+  try:
+    main()
+  except rospy.ROSInterruptExecption:
+    pass
